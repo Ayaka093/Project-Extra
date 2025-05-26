7ffa5c15dec118b5bf61d89c3b51f4d258f73693
Ayaka093 <144291285+Ayaka093@users.noreply.github.com>
2025-05-26 19:10:43 +0800

    Delete REVISI NOMOR KE GATAU directory

--- REVISI NOMOR KE GATAU/src/components/ui/card.jsx
+++
@@ -1,6 +0,0 @@
-export function Card({ children, className }) {
-  return <div className={`border rounded p-2 ${className}`}>{children}</div>;
-}
-export function CardContent({ children, className, onClick }) {
-  return <div className={className} onClick={onClick}>{children}</div>;
-}
--- REVISI NOMOR KE GATAU/src/components/ui/input.jsx
+++
@@ -1,3 +0,0 @@
-export function Input({ placeholder, value, onChange }) {
-  return <input placeholder={placeholder} value={value} onChange={onChange} className='border px-2 py-1' />;
-}
